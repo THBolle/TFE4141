3f592ae59a74bcea5d390fb23dd6fa58d8006a24
Lars Henrik Bolstad <lahebo@gmail.com>
2016-11-14 17:49:11 +0100

    fixed modular multiplier, added python simulation

--- project/Term_Project/testbenches/multMod_tb.vhd
+++ project/Term_Project/testbenches/multMod_tb.vhd
@@ -9,17 +9,17 @@ architecture testbench of multMod_tb is
     component multMod is
         generic ( width         : integer := 8 );
         port (  clk, rst, run   : in STD_LOGIC := '0';
-                busy        : out STD_LOGIC;
+                finished        : out STD_LOGIC;
                 A, B, N         : in STD_LOGIC_VECTOR (width-1 downto 0) := x"00";
                 C               : out STD_LOGIC_VECTOR (width-1 downto 0));
     end component multMod;
     
     constant width          : integer                           := 8;
     signal clk, rst, run    : STD_LOGIC                         := '0';
-    signal busy             : STD_LOGIC;
-    signal A                : STD_LOGIC_VECTOR (width-1 downto 0)   := "00000111";
-    signal B                : STD_LOGIC_VECTOR (width-1 downto 0)   := "00001010";
-    signal N                : STD_LOGIC_VECTOR (width-1 downto 0)   := "00011000";
+    signal finished             : STD_LOGIC;
+    signal A                : STD_LOGIC_VECTOR (width-1 downto 0)   := x"65";
+    signal B                : STD_LOGIC_VECTOR (width-1 downto 0)   := x"2e";
+    signal N                : STD_LOGIC_VECTOR (width-1 downto 0)   := x"ae";
     signal C                : STD_LOGIC_VECTOR (width-1 downto 0);
     constant clk_period : time := 20 ns;
     
@@ -30,7 +30,7 @@ begin
         clk => clk,
         rst => rst,
         run => run,
-        busy => busy,
+        finished => finished,
         A => A,
         B => B,
         N => N,
@@ -44,11 +44,9 @@ begin
 
     
     STIM_PROC: process begin
-        wait for clk_period;
         rst <= '1';
-        wait for clk_period;
+        wait for clk_period/2;
         rst <= '0';
-        wait for clk_period;
         run <= '1';
         wait for clk_period;
         run <= '0';
@@ -56,5 +54,6 @@ begin
     end process;
     
 end testbench;
+
 
 
