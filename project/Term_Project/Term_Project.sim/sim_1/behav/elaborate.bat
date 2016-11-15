5033a1fe22851b59d81264ddcfd14dc9dd6608db
Kristian Roaldsnes <kriroald@gmail.com>
2016-11-15 19:29:21 +0100

    removed tracking of simulation files that have previsouly been tracked. Now the gitignore should do actual ignoring

--- project/Term_Project/Term_Project.sim/sim_1/behav/elaborate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2016.2\\bin
-call %xv_path%/xelab  -wto fde4a3f3ae914b7f9aeed5827be26b3e -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L secureip --snapshot RSACoreTestBench_behav xil_defaultlib.RSACoreTestBench -log elaborate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
