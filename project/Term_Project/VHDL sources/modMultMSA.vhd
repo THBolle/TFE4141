c6a51ceb1d1bb4cdd2ca3b17af9512357736b37f
Lars Henrik Bolstad <lahebo@gmail.com>
2016-11-17 21:13:26 +0100

    changed MSAL to detect positive MSB in negative subtraction results

--- project/Term_Project/VHDL sources/modMultMSA.vhd
+++ project/Term_Project/VHDL sources/modMultMSA.vhd
@@ -1,6 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
+--use IEEE.STD_LOGIC_UNSIGNED.ALL; -- (not IEEE)
 use IEEE.NUMERIC_STD.ALL;
 --use UNISIM.VComponents.all;
 
@@ -13,15 +13,16 @@ entity modMultMSA is
 end modMultMSA;
 
 architecture Behavioral of modMultMSA is
-    signal P : STD_LOGIC_VECTOR (width+1 downto 0);
-    signal P_sa : STD_LOGIC_VECTOR (width+1 downto 0);
-    signal P_sub_n : STD_LOGIC_VECTOR (width+1 downto 0);
-    signal P_sub_2n : STD_LOGIC_VECTOR (width+1 downto 0);
+    signal C_conv : std_logic_vector (width + 2 downto 0);
+    signal P : signed (width+2 downto 0);
+    signal P_sa : signed (width+2 downto 0);
+    signal P_sub_n : signed (width+2 downto 0);
+    signal P_sub_2n : signed (width+2 downto 0);
 begin
     
-    P_sa <= P(width downto 0) & '0' + (A and (A'range => B(B_index)));
-    P_sub_n <= P_sa - n;
-    P_sub_2n <= P_sub_n - n;
+    P_sa <= signed('0' & P(width downto 0) & '0') + signed('0' & (A and (A'range => B(B_index))));
+    P_sub_n <= P_sa - signed('0' & n);
+    P_sub_2n <= P_sa - signed('0' & n(width-1 downto 0) & '0');
     
     process (rst_n, reset_C, clk)
     begin
@@ -29,17 +30,19 @@ begin
             P <= (others => '0');
         elsif rising_edge(clk) then
             if MSAL_run = '1' then
-                if P_sa < n then
-                    P <= P_sa;
-                elsif P_sub_n < n then
+                if P_sub_2n(width+2) = '0' then
+                    P <= P_sub_2n;
+                elsif P_sub_n(width+2) = '0' then
                     P <= P_sub_n;
                 else
-                    P <= P_sub_2n;
+                    P <= P_sa;
                 end if;
             end if;
         end if;
     end process;
     
-    C <= P (width-1 downto 0);
+    C_conv <= std_logic_vector(P);
+    C <= C_conv(width - 1 downto 0);
+    
 
 end Behavioral;
