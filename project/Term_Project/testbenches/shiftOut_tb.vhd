8f6ea16739711a100b19ca2c7666e06b1159f4d7
Lars Henrik Bolstad <lahebo@gmail.com>
2016-11-17 16:27:53 +0100

    added working TB for shiftOut

--- project/Term_Project/testbenches/shiftOut_tb.vhd
+++ project/Term_Project/testbenches/shiftOut_tb.vhd
@@ -7,39 +7,47 @@ entity shift_out_tb is
 end shift_out_tb;
 
 architecture Behavioural of shift_out_tb is
-    component shift_out
-    Port ( Clk              : in  STD_LOGIC;
-           Reset            : in  STD_LOGIC;
-           M                : in  STD_LOGIC_VECTOR (127 downto 0);
-           StartShiftOut    : in  STD_LOGIC;
-           DataOut          : out STD_LOGIC_VECTOR ( 31 downto 0);
-           CoreFinished     : out STD_LOGIC);
+    component shiftOut
+    Port ( clk, rst_n, startShiftOut    : in  STD_LOGIC;
+           M                            : in  STD_LOGIC_VECTOR (127 downto 0);
+           DataShiftComplete            : out STD_LOGIC;
+           DataOut                      : out STD_LOGIC_VECTOR (31 downto 0));
     end component;
     
-    signal clk : std_logic := '0';
-    signal reset : std_logic := '0';
-    signal M : std_logic_vector(127 downto 0) := x"0123456789ABCDEF";
+    signal clk, rst_n, startShiftOut : std_logic := '0';
+    signal M : std_logic_vector(127 downto 0) := x"00112233445566778899AABBCCDDEEFF";
+    signal DataShiftComplete : std_logic;
+    signal DataOut : std_logic_vector (31 downto 0);
+    
     constant clk_period : time := 20 ns;
 begin
     
-    UUT : shift_out_tb PORT MAP (
-                Clk => Clk,
-                Reset => Reset,
-                M => M                
+    UUT : shiftOut PORT MAP (
+                clk => clk,
+                rst_n => rst_n,
+                startShiftOut => startShiftOut,
+                M => M,
+                DataShiftComplete => DataShiftComplete,
+                DataOut => DataOut
             );       
     
-    CLKGEN: process(M) is
-    begin
-        Clk <= '1';
-        wait for clk_period/2 ns;
-        Clk <= '0';
-        wait for clk_peroid/2 ns;
+    CLKGEN: process begin
+        clk <= not clk;
+        wait for clk_period/2;
     end process;
         
-    STIM_PROC: process is
-    begin
-        reset <= '0';
-        wait;
+    STIM_PROC: process begin
+        wait for clk_period/2;
+        rst_n <= '1';
+        wait for clk_period/2;
+        startShiftOut <= '1';
+        wait for clk_period;
+        startShiftOut <= '0';
+        wait until DataShiftComplete <= '1';
+        wait for 2*clk_period;
+        assert true;
+            report "simulation ended"
+            severity failure;
     end process;
     
 end Behavioural;
