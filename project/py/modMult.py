9ecf77373732375d736acae024178a6ff70e9630
Lars Henrik Bolstad <lahebo@gmail.com>
2016-11-20 10:37:22 +0100

    updated python sims

--- project/py/modMult.py
+++ project/py/modMult.py
@@ -4,22 +4,27 @@ n = 0x819DC6B2574E12C3C8BC49CDD79555FD
 
 print hex(a), "*", hex(b), "mod", hex(n), "=", hex(a*b%n)
 
+A = a
 B = [int(x) for x in bin(b)[2:]]
-B = [0, 0, 0] + B
+B = [0,0,0,0] + B
+#B = reversed(B)
 print "B =", B
 
-p = 0x0
+######################################################
 
-indexes = [7,6,5,4,3,2,1,0]
-
-
-for Bj in B:
-	p = (p << 1)
-	if Bj:
-		p = p + a
-	while p>=n:
-		p = p - n
-	print Bj, hex(p)
+P = 0x0
+for B_bit in B:
+	P_sa = (P << 1) + (A * B_bit)
+	P_sub_n = P_sa - n
+	P_sub_2n = P_sa - (n << 1)
+	if P_sub_2n > 0: # (two's complement MSB = 1)
+		P = P_sub_2n
+	elif P_sub_n > 0:
+		P = P_sub_n
+	else:
+		P = P_sa
+	
+######################################################
 
 
 '''
@@ -29,7 +34,7 @@ for Bj in reversed(B):
 		p = p - n
 '''
 
-print hex(p)
+print hex(P)
 expectedValue = a*b%n
-if p == expectedValue:
+if P == expectedValue:
         print "Success, the output of the algorithm was correct"
