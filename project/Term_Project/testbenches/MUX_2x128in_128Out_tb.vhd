16cb6a4b2c0d1014e103d9bbe92868ff535872d9
Kristian Roaldsnes <kriroald@gmail.com>
2016-11-25 12:31:26 +0100

    removed comments

--- project/Term_Project/testbenches/MUX_2x128in_128Out_tb.vhd
+++ project/Term_Project/testbenches/MUX_2x128in_128Out_tb.vhd
@@ -1,35 +1,5 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 11/08/2016 09:44:04 PM
--- Design Name: 
--- Module Name: MUX_2x128in_128Out_tb - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity MUX_2x128in_128Out_tb is
 --  Port ( );
