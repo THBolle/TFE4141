a12d77ef1a9ff714fb85637251a9357ab33a69ba
Kristian Roaldsnes <kriroald@gmail.com>
2016-11-25 12:30:14 +0100

    removed comment stuff

--- project/Term_Project/testbenches/ModExp_stateMachine_tb.vhd
+++ project/Term_Project/testbenches/ModExp_stateMachine_tb.vhd
@@ -1,35 +1,8 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 11/10/2016 08:34:31 AM
--- Design Name: 
--- Module Name: ModExp_stateMachine_tb - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
 
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity ModExp_stateMachine_tb is
 --  Port ( );
