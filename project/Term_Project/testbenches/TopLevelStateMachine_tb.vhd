9ae5064b03a5e3104ac5b0759ea893a9258298ae
Kristian Roaldsnes <kriroald@gmail.com>
2016-11-25 12:33:06 +0100

    removed comments

--- project/Term_Project/testbenches/TopLevelStateMachine_tb.vhd
+++ project/Term_Project/testbenches/TopLevelStateMachine_tb.vhd
@@ -1,40 +1,10 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 11/05/2016 05:58:29 PM
--- Design Name: 
--- Module Name: TopLevelStateMachine_tb - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
 
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity TopLevelStateMachine_tb is
 end TopLevelStateMachine_tb;
-
-
 
 
 architecture Behavioral of TopLevelStateMachine_tb is
