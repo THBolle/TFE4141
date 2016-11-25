7f03d5d860b4fcb98d41a1ba0ebacd7d052dbba2
Kristian Roaldsnes <kriroald@gmail.com>
2016-11-25 12:31:54 +0100

    removed comments

--- project/Term_Project/testbenches/PISO128to1_tb.vhd
+++ project/Term_Project/testbenches/PISO128to1_tb.vhd
@@ -1,35 +1,6 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 11/08/2016 08:37:43 PM
--- Design Name: 
--- Module Name: PISO128to1_tb - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity PISO128to1_tb is
 --  Port ( );
